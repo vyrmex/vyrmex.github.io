59bcdd33b472e2e5751044ce5b30c2bc186e4fa7
vyrmex <145474043+vyrmex@users.noreply.github.com>
2023-09-19 16:02:38 +0100

    Add files via upload

---
+++ Guess.js
@@ -0,0 +1,12 @@
+var Number =  5;
+var Guess;
+
+//Guess = prompt("Guess a number between 1 and 10")
+
+//alert(Guess);
+
+function ButtonClicked() {
+    Guess = prompt("Guess a number between 1 and 10")
+    alert(Guess)
+}
+
